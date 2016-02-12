4e35e1d1128d964c4e310b1c67b450b371e8270c
Apple <tiffine.koch@gmail.com>
2016-02-11 20:53:14 -0800

    added firebase require to branch

--- routes/server.js
+++ routes/server.js
@@ -2,12 +2,23 @@
 
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+var ref = new Firebase('https://dating-appch.firebaseio.com/');
+// TEST
 var Test = require('../models/test');
+sagdhjasg
+xxxxxxxx
 
 router.post('/register', function(req, res, next) {
   console.log('inside register in server.js');
   console.log('req.body: ', req.body);
-  res.render('index');
+  ref.createUser(req.body, function(err, userData) {
+    if(err) return res.status(400).send(err);
+    User.create(userData, function(err) {
+      res.send();
+    });
+  });
+  // res.render('index');
 });
 
 router.post('/login', function(req, res, next) {
